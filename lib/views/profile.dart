71f885fcace425d933b707d1dd96c2ddb299fa50
hawani99 <hawibek223@gmail.com>
2023-03-17 02:18:35 +0300

    some UI part done for the profile page

--- lib/views/profile.dart
+++ lib/views/profile.dart
@@ -53,7 +53,40 @@ class _ProfilePageState extends State<ProfilePage> {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      body: Container(
+        height: 170.0,
+        width: double.infinity,
+        decoration: BoxDecoration(
+            image: const DecorationImage(
+              image: AssetImage("assets/images/erev/background.png"),
+              fit: BoxFit.cover,
+            ),
+            boxShadow: [new BoxShadow(color: Colors.black, blurRadius: 8.0)],
+            color: Colors.green),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(
+                  top: 60.0, bottom: 18.0, right: 18.0, left: 18.0),
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    height: 60.0,
+                    width: 60.0,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                            image:
+                                new AssetImage("assets/images/erev/admin.jpeg"),
+                            fit: BoxFit.cover)),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
